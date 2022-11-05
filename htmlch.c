f2cd0d82f88ec8cbf42a6564d51d7be90d3cc02a
nuricanacar <21060666@stu.omu.edu.tr>
2022-11-05 15:36:18 +0300

    Dosya ismi degistirildi ve olustur fonksiyonu yazildi

---
+++ htmlch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct dynamic_array{
+	int top;
+	int capacity;
+	int *array;
+}stack_t;
+
+int main(){
+
+}
+
+stack_t *create(){
+	new = (stack_t *)malloc(sizeof(stack_t));
+	if(!new)
+	  return NULL;
+	new -> top = -1;
+	new -> capacity = 1;
+	new -> array = malloc(new -> capacity * sizeof(stack_t));
+	return new;
+	if(!new -> array)
+	  return NULL;
+	return new;
+}
