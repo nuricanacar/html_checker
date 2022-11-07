5110aca5b83a6b348ce5ee056e3f409e197c2dd0
nuricanacar <21060666@stu.omu.edu.tr>
2022-11-07 11:37:51 +0300

    readfile.c islemleri tamamlandi.

--- File Read/readfile.c
+++ File Read/readfile.c
@@ -1,24 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+  
+  
+  
+ int main(void) 
+ { 
+    char coc[100][100];
+    int i = 0;
+    char chr; 
+    FILE *code; 
+    code=fopen("html.html","r"); 
+    while(chr!=EOF){ 
+        if(code != NULL){ 
+        chr = fgetc(code); 
+        while (chr == '<')
+        {
+           int a = 0;
+           while( chr != '>' && chr != ' ')
+           {
+                if ( chr != '/')
+                {
+                    coc[i][a++] = chr;
+                    chr = fgetc(code); 
+                }
+                else
+                {
+                    chr = fgetc(code); 
+                }
+            }
+            i++;
+        }
+        
+         
+    } 
+    else 
+    printf("yok"); 
 
-
-
-int main(void)
-{
-    char chr;
-    FILE *code;
-    code=fopen("html.html","r");
-    while(chr != EOF){
-   		 if(code != NULL){
-   		 	chr =  fgetc(code);
-    		printf("%c",chr);
-    }
-    	else
-     	 printf("yok");
-    
-    }
+    } 
     fclose(code);
-    
-        return 0;
-}
+    int a = -1; 
+    while(a != i)
+      printf("%s\n",coc[a++]);
 
+      printf("%d \n",!strcmp(coc[6],"<p"));
+    return 0;
+}
