93cb7cd075b929eee7ce97e655457597104653e9
nuricanacar <21060666@stu.omu.edu.tr>
2022-11-07 19:24:18 +0300

    Tum fonksiyonlar yazildi

--- htmlch.c
+++ htmlch.c
@@ -8,6 +8,14 @@ typedef struct dynamic_array{
 }stack_t;
 
 stack_t *create();
+void push();
+void sizeadder();
+char* peek();
+void pop();
+int isEmpty();
+
+void listele();
+
 
 int main(void){
 	stack_t *html_stack = create();
@@ -60,4 +68,46 @@ stack_t *create(){
 	if(!new -> array)
 	  return NULL;
 	return new;
+}
+
+void push(stack_t *S,char sarr[]){
+	sizeadder(S);
+	S -> array[++S -> top] = sarr;
+
+}
+
+void sizeadder(stack_t *S){
+	S -> capacity = S -> capacity + 1;
+	S -> array = realloc(S -> array, sizeof(S -> array) * S -> capacity);
+}
+
+char* peek(stack_t *S){
+	return S -> array[S -> top];
+}
+
+void pop(stack_t *S){
+	S -> top--;
+}
+
+int isEmpty(stack_t *S){
+    return S -> top == -1;
+}
+
+
+
+
+
+
+
+
+
+
+
+void listele(stack_t *S){
+        int temp = S -> top;
+        while(temp != -1){
+        	printf("%d ", S -> array[temp]);
+        	temp = temp - 1;
+		}
+		printf("\n");
 }
