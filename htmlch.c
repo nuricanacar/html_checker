c2c780af2022d5d364d453654494e16218055aed
nuricanacar <21060666@stu.omu.edu.tr>
2022-11-07 11:59:38 +0300

    readfile, htmlch.c icine entegre edildi

--- htmlch.c
+++ htmlch.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
 typedef struct dynamic_array{
 	int top;
@@ -6,14 +7,50 @@ typedef struct dynamic_array{
 	int *array;
 }stack_t;
 
-stack_t *create()
+stack_t *create();
 
-int main(){
+int main(void){
 	stack_t *html_stack = create();
+	char coc[100][100];
+		int i = 0;
+		char chr; 
+		FILE *code; 
+		code=fopen("html.html","r"); 
+		while(chr!=EOF){ 
+			if(code != NULL){ 
+			chr = fgetc(code); 
+			while (chr == '<')
+			{
+			int a = 0;
+			while( chr != '>' && chr != ' ')
+			{
+					if ( chr != '/')
+					{
+						coc[i][a++] = chr;
+						chr = fgetc(code); 
+					}
+					else
+					{
+						chr = fgetc(code); 
+					}
+				}
+				i++;
+			}
+			
+			
+		} 
+		else 
+		printf("yok"); 
+
+		} 
+		fclose(code);
+		int a = -1; 
+		while(a != i)	
+		printf("%s\n",coc[++a]);
 }
 
 stack_t *create(){
-	new = (stack_t *)malloc(sizeof(stack_t));
+	stack_t *new = (stack_t *)malloc(sizeof(stack_t));
 	if(!new)
 	  return NULL;
 	new -> top = -1;
